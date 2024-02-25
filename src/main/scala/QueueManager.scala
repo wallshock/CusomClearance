7bb9cfe91b75039f29fa4dd3f75ededcaee1814a
wallshock <87365865+wallshock@users.noreply.github.com>
2024-02-26 00:40:02 +0100

    algorithm work

--- src/main/scala/QueueManager.scala
+++ src/main/scala/QueueManager.scala
@@ -5,16 +5,38 @@ import scala.collection.mutable.ListBuffer
 
 class QueueManager(queues: ListBuffer[Queue]) {
   def optimizeQueues(): Unit = {
+    //we calculate minimum and maximum size of queue
     val minSize = queues.minBy(_.size).size
     val maxSize = queues.maxBy(_.size).size
+    
+    //if we have different sizes we perform optimization
     if (minSize != maxSize) {
       val shortestQueues = queues.filter(_.size == minSize)
       val longestQueues = queues.filter(_.size == maxSize)
-
-      if theres more shortest than longest take max values from longques
-      else if theres more longest take max from shortest
-    }
-
+      val shorterSize = shortestQueues.length
+      val longerSize = shortestQueues.length
+      
+      //if we dont have same ammount of long and short queues we need to pick
+      //the best ones that will give us the most profit
+      
+      if(shorterSize != longerSize){
+        if(shorterSize > longerSize) {
+          for(i <- 1 until minSize) {
+            //we take shortQueues with lowestElements
+            //this is Queue class so give it apply(i)
+            val queuesToSwap = shortestQueues.sortBy(_(i)).take(longerSize)
+            
+          }
+        } else {
+          for (i <- 1 until minSize) {
+            //we take longQueues with highestElements
+            //this is Queue class so give it apply(i)
+            val queuesToSwap = longestQueues.sortBy(-_(i)).take(shorterSize)
+          }
+        }
+      }else {
+          
+      }
     }
   }
 
